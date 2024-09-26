5ad2044100508a5f0b10eefd54c9498cf404a164
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-09-26 22:22:34 +0300

    edit getInfo

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -16,8 +16,11 @@ export const authorization = async (req, res, next) => {
     throw HttpError(401, "authorization header must be 'Bearer'");
   }
   try {
+    console.log(token);
     const { id } = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(id);
+    console.log(user);
+    
     if (!user || !user.token || user.token !== token) {
       throw HttpError(401, "user not found");
     }
