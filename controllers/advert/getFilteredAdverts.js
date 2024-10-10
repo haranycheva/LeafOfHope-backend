ef14c5cea3bd2ff2558d63b4003db9a46ef15f40
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-10-10 15:40:40 +0300

    Update getFilteredAdverts.js

--- controllers/advert/getFilteredAdverts.js
+++ controllers/advert/getFilteredAdverts.js
@@ -3,15 +3,9 @@ import { Advert } from "../../models/Advert.js";
 
 const getFilteredAdverts = async (req, res, next) => {
   const { page = 1, limit = 10 } = req.query;
-  // const whatToFilter = {};
-  // for (const prop in req.body) {
-  //   if (req.body[prop]) {
-  //     whatToFilter[prop] = req.body[prop];
-  //   }
-  // }
-  const whatToFilter = {...req.query}
-  delete whatToFilter.page
-  delete whatToFilter.limit
+  const whatToFilter = { ...req.query };
+  delete whatToFilter.page;
+  delete whatToFilter.limit;
   const totalAdverts = await Advert.countDocuments(whatToFilter);
   const result = await Advert.find(whatToFilter, "", {
     skip: (page - 1) * limit,
@@ -20,6 +14,7 @@ const getFilteredAdverts = async (req, res, next) => {
   if (!result) {
     throw HttpError(400, `Not found`);
   }
-  res.json({ result: result, tottal: totalAdverts });
+  const sortedByDate = result.sort((a,b) => Date.parse(b.createdAt) -  Date.parse(a.createdAt))
+  res.json({ result: sortedByDate, tottal: totalAdverts });
 };
 export default getFilteredAdverts;
