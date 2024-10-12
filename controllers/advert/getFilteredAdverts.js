d3a1516191b2bd220cfeb3922e751cface058562
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-10-12 20:04:52 +0300

    add search

--- controllers/advert/getFilteredAdverts.js
+++ controllers/advert/getFilteredAdverts.js
@@ -2,16 +2,26 @@ import { HttpError, sortAdverts } from "../../helpers/index.js";
 import { Advert } from "../../models/Advert.js";
 
 const getFilteredAdverts = async (req, res, next) => {
-  const { page = 1, limit = 10, sort = "", ...whatToFilter } = req.query;
-  const totalAdverts = await Advert.countDocuments(whatToFilter);
-  const result = await Advert.find(whatToFilter, "", {
+  const {
+    page = 1,
+    limit = 10,
+    name = "",
+    sort = "",
+    ...whatToFilter
+  } = req.query;
+  const nameReg = new RegExp(name, "i");
+  const totalAdverts = await Advert.countDocuments({
+    ...whatToFilter,
+    name: nameReg,
+  });
+  const result = await Advert.find({ ...whatToFilter, name: nameReg }, "", {
     skip: (page - 1) * limit,
     limit,
   });
   if (!result) {
     throw HttpError(400, `Not found`);
   }
-  const sortedAdverts = sortAdverts(result, sort)
+  const sortedAdverts = sortAdverts(result, sort);
   res.json({ result: sortedAdverts, tottal: totalAdverts });
 };
 export default getFilteredAdverts;
