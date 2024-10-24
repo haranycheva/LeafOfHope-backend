5fc8e0f97b1206d87cbbe4f00c53911b387628c9
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-10-24 16:54:46 +0300

    .

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import advertRouter from "./routes/advert-router.js";
 import authRouter from "./routes/auth-router.js";
+import swaggerjsdoc from "swagger-jsdoc";
+import swaggerui from "swagger-ui-express";
 
 const app = express();
 
@@ -12,6 +14,12 @@ app.use(express.json());
 app.use("/api/advert", advertRouter);
 app.use("/api/auth", authRouter);
 
+// const options = {
+
+// }
+// const spacs = swaggerjsdoc();
+
+// app.use("/api/docs", swaggerui.serve, swaggerui.setup(spacs));
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status ? err.status : 500).json({ message: err.message });
