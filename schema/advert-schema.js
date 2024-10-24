b9d4619ab0eaf36be3b10500a7f7188cf6e96018
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-10-24 16:45:45 +0300

    Update advert-schema.js

--- schema/advert-schema.js
+++ schema/advert-schema.js
@@ -3,10 +3,10 @@ import { pictureSchema } from "./picture-schema.js";
 
 export const advertValidationSchema = Joi.object({
   name: Joi.string().min(1).max(28).required(),
-  description: Joi.string().optional(),
+  description: Joi.string(),
   way: Joi.string().valid("exchange", "give").required(),
-  wish: Joi.string().optional(),
-  image: pictureSchema.optional(),
+  wish: Joi.string(),
+  image: pictureSchema,
   light: Joi.string().valid("many", "normal", "little").required(),
   humidity: Joi.string().valid("high", "normal", "low").required(),
   lifeDuration: Joi.string()
@@ -15,12 +15,12 @@ export const advertValidationSchema = Joi.object({
   temperature: Joi.string().valid("high", "normal", "low").required(),
   size: Joi.string().valid("large", "medium", "small").required(),
   alergenicity: Joi.boolean().required(),
-  attention: Joi.string().valid("many", "normal", "little", "").optional(),
-  survive: Joi.string().valid("high", "normal", "low", "").optional(),
-  state: Joi.string().valid("good", "bad", "enough", "").optional(),
-  flowering: Joi.boolean().allow('').optional(),
-  growthRate: Joi.string().valid("high", "normal", "low", "").optional(),
-  edible: Joi.boolean().allow('').optional(),
+  attention: Joi.string().valid("many", "normal", "little", ""),
+  survive: Joi.string().valid("high", "normal", "low", ""),
+  state: Joi.string().valid("good", "bad", "enough", ""),
+  flowering: Joi.boolean().allow(''),
+  growthRate: Joi.string().valid("high", "normal", "low", ""),
+  edible: Joi.boolean().allow(''),
 });
 
 export const editAdvertSchema = Joi.object({
