789d0d14db490c43d18c06f4e7f55077de0e84af
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-02-06 14:08:05 +0200

    Update addFiltrationStats.js

--- helpers/statistics/addFiltrationStats.js
+++ helpers/statistics/addFiltrationStats.js
@@ -2,6 +2,9 @@ import { filtrationStatistics } from "../../models/filtrationStatistics.js"
 import {HttpError} from "../index.js"
 
 const addFiltrationStats = async(filtration) => {
+    if(Object.keys(filtration).length === 0){
+        return
+    }
     const newFiltrashionStats = await filtrationStatistics.create({...filtration})
     if(!newFiltrashionStats){
         throw HttpError(500, "could not write filtration statistics")
