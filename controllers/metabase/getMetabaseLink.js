ecafc9fc2ee419d0c690fa40f814b7bf960f46ef
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-02-07 22:14:02 +0200

    Update getMetabaseLink.js

--- controllers/metabase/getMetabaseLink.js
+++ controllers/metabase/getMetabaseLink.js
@@ -3,19 +3,17 @@ import jwt from "jsonwebtoken";
 
 const { METABASE_SITE_URL, METABASE_SECRET_KEY } = process.env;
 
-const payload = {
-  resource: { dashboard: 33 },
-  params: {},
-  exp: Math.round(Date.now() / 1000) + 10 * 60,
-};
-
 const getMetabaseLink = (req, res, next) => {
   const { role } = req.user;
   if (role != "admin") {
     throw HttpError(403, "Only admin user can see statistics");
   }
+  const payload = {
+    resource: { dashboard: 33 },
+    params: {},
+    exp: Math.round(Date.now() / 1000) + 10 * 60,
+  };
   const token = jwt.sign(payload, METABASE_SECRET_KEY);
-
   const iframeUrl =
     METABASE_SITE_URL +
     "/embed/dashboard/" +
