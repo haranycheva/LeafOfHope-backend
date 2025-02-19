b6401cbbc0f09e7b689c5879e3ffcf8d2beac17a
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-02-19 12:03:02 +0200

    Update user-schema.js

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -6,13 +6,13 @@ export const userValidationSchema = Joi.object({
   password: Joi.string().min(6).required(),
   email: Joi.string().pattern(emailReg).required(),
   phone: Joi.string().pattern(phoneReg).allow(""),
-  adress: Joi.string().max(60),
+  adress: Joi.string().max(60).allow(""),
 });
 
 export const editUserSchema = Joi.object({
     username: Joi.string().min(1).max(40),
-    phone: Joi.string().pattern(phoneReg),
-    adress: Joi.string().max(60),
+    phone: Joi.string().pattern(phoneReg).allow(""),
+    adress: Joi.string().max(60).allow(""),
   });
 
   export const signinValidationSchema = Joi.object({
